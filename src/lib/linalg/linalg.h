4abf97e2bbd9d83bb9cfdf2582766127214e9f34
ETHIY <oussama.ennafii@polytechnique.org>
2017-05-07 22:13:31 +0200

    sketch for core

---
+++ src/lib/linalg/linalg.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace ml
+{
+    namespace core
+    {
+        namespace linalg
+        {
+            template<typename T, std::size_t size>
+            class vector;
+            template<typename T, std::size_t height, std::size_t width>
+            class matrix;
+            template<typename T, std::size_t size>
+            class sparse_vector;
+            template<typename T, std::size_t height, std::size_t width>
+            class sparse_matrix;
+        }
+    }
+}
