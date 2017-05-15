3617efca5fa770ac2b19fbddf323ab5e9bb9b3a2
ETHIY <oussama.ennafii@polytechnique.org>
2017-05-15 20:50:50 +0200

    new new new

--- ci/circle/dependencies.sh
+++ ci/circle/dependencies.sh
@@ -2,7 +2,6 @@
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq -y
-sudo apt-get upgrade -y -qq
 sudo apt-get install build-essential g++-4.8 -y
 sudo apt-get install cmake -y
 sudo apt-get install libeigen3-dev -y
