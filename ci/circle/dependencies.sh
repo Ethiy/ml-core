d69d73596085e74c981318a5f97b778ed65a297d
ETHIY <oussama.ennafii@polytechnique.org>
2017-05-16 20:34:42 +0200

    wget -> curl

--- ci/circle/dependencies.sh
+++ ci/circle/dependencies.sh
@@ -4,5 +4,5 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq -y
 sudo apt-get install build-essential g++-4.8 -y
 sudo apt-get install libeigen3-dev -y
-wget https://cmake.org/files/v3.8/cmake-3.8.1-Linux-x86_64.sh
+curl -o https://cmake.org/files/v3.8/cmake-3.8.1-Linux-x86_64.sh
 sudo sh ./cmake-3.8.1-Linux-x86_64.sh
